d1f51cd25c4ea8174b7c6e9da08c0c7df1abe90d
Kaleb <kaleb792762@gmail.com>
2023-08-31 15:17:33 +0530

    FinalSubmission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -18,8 +18,7 @@ public class Main {
     public static void main(String[] args) {
       B obj = new B();
       obj.meth();
-      B obj1 = new B();
-      obj1.meth();
+      obj.meth();
 
     }
 
